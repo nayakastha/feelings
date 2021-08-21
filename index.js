2e53fa6b0feb3693c9c0b03f002b5045b10c2b3f
Astha Nayak <nayakastha2911@gmail.com>
2021-08-21 16:43:56 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+  
+
+client.login(process.env.BOT_TOKEN)
